c1e4b6b5006980fa7de3200d8a090f88cc7087ec
Theosaurus-Rex <theosaurus13@gmail.com>
2021-05-06 09:58:44 +1000

    create HTML scaffold and basic script

---
+++ script.js
@@ -0,0 +1,13 @@
+const deepai = require('deepai')
+let input = document.getElementById("text-input").value
+let generateButton = document.getElementById("generate-button")
+generateButton.addEventListener("click", generate)
+
+deepai.setApiKey('2b97a5e7-1e72-4c35-96bc-038a43bdcafc')
+
+(async function generate() {
+    var response = await deepai.callStandardApi("text2img", {
+            text: input,
+    });
+    console.log(response);
+})()
