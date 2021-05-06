521c0f4c210f9e7d949940efe2eee151c780f969
kaprestridge <kaprestridge@gmail.com>
2021-05-06 12:50:08 +1000

    added comments

--- script.js
+++ script.js
@@ -3,23 +3,21 @@ const button = document.getElementById("generate-button")
 const apiKey = "2b97a5e7-1e72-4c35-96bc-038a43bdcafc"
 deepai.setApiKey(apiKey)
 
-// deepai.callStandardApi("sentiment-analysis", {
-//   text: "I'm really happy today!"
-// }).then(res => console.log(res))
-
 // Generate an image from input string when button is clicked
 button.addEventListener("click", generateImage)
 
 // Feed string to API as text input
 function generateImage() {
+    //Get text input from user
     const textInput = document.getElementById("text-input").value
-    console.log(textInput)
     
+    //Makes call to the api and returns the image url. Once returns passes response into 
+    //updateImageUrl which updates the image source on the main page
     deepai.callStandardApi("text2img", {
           text: textInput
         }).then(res => updateImageUrl(res))
         
-
+    //updates image url from api response
     function updateImageUrl(url){
         document.getElementById("generated-image").src = url.output_url
     }
