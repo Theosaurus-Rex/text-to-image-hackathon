34fc901bc5854bd3cf9b820c2ff4aaf5d31c580f
kaprestridge <kaprestridge@gmail.com>
2021-05-06 11:49:05 +1000

    api working

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-const deepai = require('deepai')
-let input = document.getElementById("text-input").value
-const generateButton = document.getElementById("generate-button")
-generateButton.addEventListener("click", generate)
-
 deepai.setApiKey('2b97a5e7-1e72-4c35-96bc-038a43bdcafc')
 
-(async function generate() {
-    var response = await deepai.callStandardApi("text2img", {
-            text: input,
-    });
-    console.log(response);
-})()
